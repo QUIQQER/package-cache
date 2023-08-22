c8b42644517a0076f9fbeb2f4e0a1f8c94f6b463
Jan Wennrich <jan.wennrich@pcsg.de>
2023-08-22 18:16:38 +0200

    fix: turn anonymous "LoggedInCookie for GDPR"-class to real class
    The anonymous class caused a "forbidden serialization"-error
    (I did not understand why, but maybe some collection or cache magic is the cause)

--- src/QUI/Cache/CookieProvider.php
+++ src/QUI/Cache/CookieProvider.php
@@ -2,66 +2,18 @@
 
 namespace QUI\Cache;
 
-use QUI;
 use QUI\Cache\Cookie\LoggedInCookie;
 use QUI\GDPR\CookieCollection;
-use QUI\GDPR\CookieInterface;
 use QUI\GDPR\CookieProviderInterface;
+use QUI\Cache\Cookie\LoggedInCookieForGdpr;
 
 class CookieProvider implements CookieProviderInterface
 {
     public static function getCookies(): CookieCollection
     {
-        return new CookieCollection([static::provideLoggedInCookieForGdpr()]);
-    }
+        $loggedInCookie = new LoggedInCookie(Config::getLoggedInCookieName());
+        $loggedInCookieForGdpr = new LoggedInCookieForGdpr($loggedInCookie);
 
-    /**
-     * Provides a class that implements quiqqer/gdpr CookieInterface based on the LoggedInCookie class
-     *
-     * The LoggedInCookie class itself does not implement CookieInterface.
-     * It can't because otherwise it would depend on quiqqer/gdpr.
-     */
-    protected static function provideLoggedInCookieForGdpr(): CookieInterface
-    {
-        return new class implements CookieInterface {
-            private LoggedInCookie $loggedInCookie;
-
-            public function __construct()
-            {
-                $this->loggedInCookie = new LoggedInCookie(Config::getLoggedInCookieName());
-            }
-
-            public function getCategory(): string
-            {
-                return static::COOKIE_CATEGORY_ESSENTIAL;
-            }
-
-            public function getOrigin(): string
-            {
-                return QUI::getRequest()->getHost();
-            }
-
-            public function getPurpose(): string
-            {
-                return QUI::getLocale()->get(
-                    'quiqqer/cache',
-                    'cookie.logged_in.purpose'
-                );
-            }
-
-            public function getLifetime(): string
-            {
-                return \sprintf(
-                    '%d %s',
-                    $this->loggedInCookie->getLifetimeInSeconds(),
-                    QUI::getLocale()->get('quiqqer/quiqqer', 'seconds')
-                );
-            }
-
-            public function getName(): string
-            {
-                return $this->loggedInCookie->getName();
-            }
-        };
+        return new CookieCollection([$loggedInCookieForGdpr]);
     }
 }
