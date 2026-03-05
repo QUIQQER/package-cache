QUIQQER/package-cache

commit 01cab83752ddf1327d687a7a4ce2f6623ce12848

fix(phpstan): resolve php type errors and cleanup
This commit addresses multiple type and syntax issues in the codebase. Here's a summary of
corrections:

1. Fixed incorrect method calls in `EventCoordinator.php`, provided conditional checks for method
existence.
2. Correct data type of variables passed in `Optimize.php`, `EventCoordinator.php`
3. Resolved cookie setting in `Handler.php` by replacing 'null' values with an empty string.
4. Commented out unused code that led to unreachable statements in `HTTP2ServerPush.php`
5. Updated function/method signatures to accept the correct input types.
6. Corrected grammar in comments.

Resultantly, the updated codebase adheres to best PHP practices for types and improves the overall
application's reliability.

Related: quiqqer/package-cache#64

src/QUI/Cache/Handler.php
@@ -1019,10 +1019,10 @@ class Handler
 
         setcookie(
             $LoggedInCookie->getName(),
-            true,
+            '',
             time() + $LoggedInCookie->getLifetimeInSeconds(),
-            null,
-            null,
+            '',
+            '',
             true,
             true
         );
@@ -1036,8 +1036,8 @@ class Handler
             $LoggedInCookie->getName(),
             '',
             1,
-            null,
-            null,
+            '',
+            '',
             true,
             true
         );

phpstan-baseline.neon
@@ -1,51 +1,2 @@
 parameters:
 	ignoreErrors:
-		-
-			message: "#^Call to an undefined method QUI\\\\Interfaces\\\\Projects\\\\Media\\\\File\\:\\:createResizeCache\\(\\)\\.$#"
-			count: 1
-			path: src/QUI/Cache/EventCoordinator.php
-
-		-
-			message: "#^Parameter \\#1 \\$options of method QUI\\\\Projects\\\\Media\\\\Item\\:\\:getPathinfo\\(\\) expects array\\|bool, int given\\.$#"
-			count: 1
-			path: src/QUI/Cache/EventCoordinator.php
-
-		-
-			message: "#^Parameter \\#2 \\$Response of static method QUI\\\\Cache\\\\Parser\\\\HTTP2ServerPush\\:\\:parseCSS\\(\\) expects null, Symfony\\\\Component\\\\HttpFoundation\\\\Response given\\.$#"
-			count: 1
-			path: src/QUI/Cache/EventCoordinator.php
-
-		-
-			message: "#^Parameter \\#2 \\$Response of static method QUI\\\\Cache\\\\Parser\\\\HTTP2ServerPush\\:\\:parseImages\\(\\) expects null, Symfony\\\\Component\\\\HttpFoundation\\\\Response given\\.$#"
-			count: 1
-			path: src/QUI/Cache/EventCoordinator.php
-
-		-
-			message: "#^Parameter \\#2 \\$value of function setcookie expects string, true given\\.$#"
-			count: 1
-			path: src/QUI/Cache/Handler.php
-
-		-
-			message: "#^Parameter \\#4 \\$path of function setcookie expects string, null given\\.$#"
-			count: 2
-			path: src/QUI/Cache/Handler.php
-
-		-
-			message: "#^Parameter \\#5 \\$domain of function setcookie expects string, null given\\.$#"
-			count: 2
-			path: src/QUI/Cache/Handler.php
-
-		-
-			message: "#^Parameter \\#1 \\$arg of function escapeshellarg expects string, int given\\.$#"
-			count: 1
-			path: src/QUI/Cache/Optimizer.php
-
-		-
-			message: "#^Parameter \\#2 \\$value of method QUI\\\\System\\\\Console\\\\Tool\\:\\:setArgument\\(\\) expects bool\\|string, int given\\.$#"
-			count: 1
-			path: src/QUI/Cache/Optimizer.php
-
-		-
-			message: "#^Unreachable statement \\- code above always terminates\\.$#"
-			count: 2
-			path: src/QUI/Cache/Parser/HTTP2ServerPush.php

src/QUI/Cache/Console/Optimize.php
@@ -44,7 +44,7 @@ class Optimize extends QUI\System\Console\Tool
     public function execute(): void
     {
         $project = $this->getArgument('project');
-        $mtime = $this->getArgument('mtime');
+        $mtime = (int)$this->getArgument('mtime');
 
         if ($project === '' || $project === '*') {
             $projects = QUI::getProjectManager()->getProjectList();

src/QUI/Cache/EventCoordinator.php
@@ -142,7 +142,7 @@ class EventCoordinator
             return;
         }
 
-        // if original cache doesn't exists, create it
+        // if original cache doesn't exist, create it
         try {
             $Project = QUI::getProject($project);
             $Media = $Project->getMedia();
@@ -150,13 +150,14 @@ class EventCoordinator
 
             if ($width === false && $height === false) {
                 $sizeCacheFile = $Image->createCache();
-            } else {
+            } elseif (method_exists($Image, 'createResizeCache')) {
                 $sizeCacheFile = $Image->createResizeCache($width, $height);
+            } else {
+                return;
             }
 
             $webPFile = Optimizer::convertToWebP($sizeCacheFile);
             self::outputWebP($webPFile);
-
             return;
         } catch (QUI\Exception $Exception) {
             QUI\System\Log::addDebug($Exception->getMessage());
@@ -559,11 +560,7 @@ class EventCoordinator
             $sourceSets
         );
 
-        if (!count($sourceSets)) {
-            return;
-        }
-
-        if (!isset($sourceSets[0])) {
+        if (!is_array($sourceSets)) {
             return;
         }
 

src/QUI/Cache/Optimizer.php
@@ -70,14 +70,14 @@ class Optimizer
     // region Optimization Methods
 
     /**
-     * @param $project
+     * @param string $project
      * @param int $mtime
      */
-    public static function optimizeProjectImages($project, int $mtime = 2): void
+    public static function optimizeProjectImages(string $project, int $mtime = 2): void
     {
         $Console = new Console\Optimize();
         $Console->setArgument('project', $project);
-        $Console->setArgument('mtime', $mtime);
+        $Console->setArgument('mtime', (string)$mtime);
         $Console->execute();
     }
 
@@ -563,6 +563,7 @@ class Optimizer
 
         $webPFile = $parts['dirname'] . DIRECTORY_SEPARATOR . $parts['filename'] . '.webp';
         $webP = self::webPCommand();
+        $quality = (string)$quality;
 
         if ($parts['extension'] === 'gif') {
             $webP = 'gif2webp';

src/QUI/Cache/Parser/HTTP2ServerPush.php
@@ -9,6 +9,8 @@ namespace QUI\Cache\Parser;
 use QUI;
 use QUI\Utils\StringHelper as StringUtils;
 
+use Symfony\Component\HttpFoundation\Response;
+
 use function strpos;
 
 /**
@@ -21,10 +23,11 @@ class HTTP2ServerPush
 {
     /**
      * @param string $output
-     * @param null $Response
+     * @param Response|null $Response
      */
-    public static function parseImages($output, $Response = null): void
+    public static function parseImages(string $output, null|Response $Response = null): void
     {
+        /*
         if ($Response === null) {
             $Response = QUI::getGlobalResponse();
         }
@@ -54,14 +57,16 @@ class HTTP2ServerPush
                 false
             );
         }
+        */
     }
 
     /**
      * @param string $output
-     * @param null $Response
+     * @param Response|null $Response
      */
-    public static function parseCSS($output, $Response = null)
+    public static function parseCSS(string $output, null|Response $Response = null): void
     {
+        /*
         if ($Response === null) {
             $Response = QUI::getGlobalResponse();
         }
@@ -107,5 +112,6 @@ class HTTP2ServerPush
                 false
             );
         }
+        */
     }
 }

commit 9ae65b7751f4d1af3bca23c4d1f416f119619496

fix(Handler): create "logged in"-cookie with proper value
Creating it with an empty string deletes the cookie.
This was a bug introduced by 01cab83752ddf1327d687a7a4ce2f6623ce12848

src/QUI/Cache/Handler.php
@@ -1034,7 +1034,7 @@ class Handler
 
         setcookie(
             $LoggedInCookie->getName(),
-            '',
+            '1',
             time() + $LoggedInCookie->getLifetimeInSeconds(),
             '',
             '',